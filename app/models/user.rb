77d3833e46383bcc0ba17caabd6b15e43225087c
Silvia Grrr <silvia.granato@gmail.com>
2020-04-08 17:09:04 +0200

    User-validations implemented.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    validates :username, presence: true,  uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25 }
 
+    VALID_EMAIL_FORMAT = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 125 }, format: { with: VALID_EMAIL_FORMAT }
 end
